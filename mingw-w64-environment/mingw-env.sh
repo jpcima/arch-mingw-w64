6a7ef9a380209a8d6e783dfbab8fd27c92e0a1e7
Jean Pierre Cimalando <jp-dev@inbox.ru>
2019-12-20 00:38:48 +0100

    do not use _FORTIFY_SOURCE, it lacks support from compiler

--- mingw-w64-environment/mingw-env.sh
+++ mingw-w64-environment/mingw-env.sh
@@ -2,7 +2,7 @@
 
 _arch=$1
 
-default_mingw_pp_flags="-D_FORTIFY_SOURCE=2"
+default_mingw_pp_flags="-D_FORTIFY_SOURCE=0"
 default_mingw_compiler_flags="$default_mingw_pp_flags -O2 -pipe -fno-plt -fexceptions --param=ssp-buffer-size=4"
 default_mingw_linker_flags="-Wl,-O1,--sort-common,--as-needed -fstack-protector"
 
