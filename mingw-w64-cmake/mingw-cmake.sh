b21084f14c90713ba1816eda8f35303ae0ae6b88
Jean Pierre Cimalando <jp-dev@inbox.ru>
2021-12-06 15:57:45 +0100

    Updates

--- mingw-w64-cmake/mingw-cmake.sh
+++ mingw-w64-cmake/mingw-cmake.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 
-. mingw-env @TRIPLE@
+. /usr/bin/mingw-env @TRIPLE@
 
 mingw_prefix=/usr/@TRIPLE@
 
-export PATH="${mingw_prefix}/bin:$PATH"
-
-case "$1" in
-    -E|--build|--install|--open|--find-package|--help)
-        exec cmake "$@"
-        ;;
-    *)
-        exec cmake \
-             -DCMAKE_INSTALL_PREFIX:PATH=${mingw_prefix} \
-             -DCMAKE_INSTALL_LIBDIR:PATH=lib \
-             -DCMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES:PATH=${mingw_prefix}/include \
-             -DCMAKE_C_IMPLICIT_INCLUDE_DIRECTORIES:PATH=${mingw_prefix}/include \
-             -DBUILD_SHARED_LIBS:BOOL=ON \
-             -DCMAKE_TOOLCHAIN_FILE=/usr/share/mingw/toolchain-@TRIPLE@.cmake \
-             -DCMAKE_CROSSCOMPILING_EMULATOR=/usr/bin/@TRIPLE@-wine \
-             "$@"
-        ;;
-esac
+PATH=${mingw_prefix}/bin:$PATH cmake \
+    -DCMAKE_INSTALL_PREFIX:PATH=${mingw_prefix} \
+    -DCMAKE_INSTALL_LIBDIR:PATH=lib \
+    -DCMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES:PATH=${mingw_prefix}/include \
+    -DCMAKE_C_IMPLICIT_INCLUDE_DIRECTORIES:PATH=${mingw_prefix}/include \
+    -DBUILD_SHARED_LIBS:BOOL=ON \
+    -DCMAKE_TOOLCHAIN_FILE=/usr/share/mingw/toolchain-@TRIPLE@.cmake \
+    -DCMAKE_CROSSCOMPILING_EMULATOR=/usr/bin/@TRIPLE@-wine \
+    "$@"
